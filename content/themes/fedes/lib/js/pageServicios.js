4220e0befd26402c998aa1f54ae88dcbcbed8be0
Jhymer Martínez <jmartinez@ioet.com>
2016-05-22 11:42:40 -0500

    Refactor code

--- content/themes/fedes/lib/js/pageServicios.js
+++ content/themes/fedes/lib/js/pageServicios.js
@@ -1,10 +1,9 @@
-(function () {
+(function ($) {
   var parentId = '#gallery-servicios';
-
   $(parentId + ' .collapse').on('show.bs.collapse', function (e) {
     var actives = $(parentId).find('.in, .collapsing');
     actives.each(function (index, element) {
       $(element).collapse('hide');
-    })
-  })
-})();
+    });
+  });
+})(jQuery);
