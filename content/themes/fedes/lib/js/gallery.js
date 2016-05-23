33f5acb7edb913e8913620393481fe9eea0ba0da
Jhymer Martínez <jmartinez@ioet.com>
2016-05-22 23:27:45 -0500

    Rename gallery ID

--- content/themes/fedes/lib/js/gallery.js
+++ content/themes/fedes/lib/js/gallery.js
@@ -1,5 +1,5 @@
 (function ($) {
-  var parentId = '#gallery-servicios';
+  var parentId = '#gallery-fedes';
   $(parentId + ' .collapse').on('show.bs.collapse', function (e) {
     var actives = $(parentId).find('.in, .collapsing');
     actives.each(function (index, element) {
