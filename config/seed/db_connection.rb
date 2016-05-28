41c219fc0c027a149b689bc94cf41948df1d3d42
Mario Andrés Correa <a@macool.me>
2016-05-27 22:21:11 -0500

    support DATABASE_URL

--- config/seed/db_connection.rb
+++ config/seed/db_connection.rb
@@ -4,23 +4,26 @@ class Seed
   class DbConnection
     def initialize(environment)
       @environment = environment
+      configure!
     end
 
     def connect!
       require(db_adapter)
       ActiveRecord::Base.establish_connection(
-        adapter: db_adapter,
-        database: Dir.pwd + "/content/data/ghost-dev.db"
+        adapter: @db_adapter,
+        database: @database_url
       )
     end
 
     private
 
-    def db_adapter
+    def configure!
       if @environment == "development"
-        "sqlite3"
+        @db_adapter = "sqlite3"
+        @database_url = Dir.pwd + "/content/data/ghost-dev.db"
       else
-        "pg"
+        @db_adapter = "pg"
+        @database_url = ENV["DATABASE_URL"]
       end
     end
   end
