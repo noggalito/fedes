01993e56bae41ef7d4f6c19adae75f6c9ee05757
Jhymer Martínez <jmartinez@ioet.com>
2016-06-15 10:55:35 -0500

    Add responsive styles

--- content/themes/fedes/lib/js/blog.js
+++ content/themes/fedes/lib/js/blog.js
@@ -1,6 +1,6 @@
 (function ($) {
   var url = window.location.href;
-  var tab = $('.nav-blog .nav li a').filter(function () {
+  var tab = $('.blog-nav .nav li a').filter(function () {
     if (this.href === url) {
       return true;
     } else {
