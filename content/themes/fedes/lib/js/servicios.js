928ecfbdca77d4a7880fc10ea2db8b3c9f8c9fb6
Sjlmejia <jmejia@ioet.com>
2016-06-26 22:31:26 -0500

    Include arrows service for images

--- content/themes/fedes/lib/js/servicios.js
+++ content/themes/fedes/lib/js/servicios.js
@@ -7,5 +7,15 @@ $( document ).ready(function() {
     $(".serviceList .gallery-element").filter(function( index ) {
       return index === 0;
     }).css("border-left" ,"1px solid #003c71");
+
+    $(".container.gallery-principal .serviceDetail .arrow").filter(function( index ) {
+      return index === 1;
+    }).addClass("top-arrow");
+
+    $(".container.gallery-principal .serviceDetail .arrow").filter(":first").addClass("top-arrow-left");
+
+    $(".container.gallery-principal .serviceDetail .arrow").filter(function( index ) {
+      return index === 2;
+    }).addClass("top-arrow-right");
   }
 });
