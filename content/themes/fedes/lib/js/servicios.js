93f5af7c3318b93b86374005493b1ce07f26eb6c
Sjlmejia <jmejia@ioet.com>
2016-06-26 20:18:25 -0500

    Fix img-Services

---
+++ content/themes/fedes/lib/js/servicios.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+  if(window.matchMedia('(min-width: 768px)').matches){
+    $(".serviceList section").removeClass("col-xs-offset-2")
+
+    $(".serviceList .gallery-element").filter(function( index ) {
+      return index === 0;
+    }).css("border-left" ,"1px solid #003c71");
+  }
+});
