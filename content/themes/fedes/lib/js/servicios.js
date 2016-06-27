eeb6c212b221f080372bb8e068086436dc42ca75
Sjlmejia <jmejia@ioet.com>
2016-06-26 21:13:05 -0500

    Fix servicios for carousel_tablet

--- content/themes/fedes/lib/js/servicios.js
+++ content/themes/fedes/lib/js/servicios.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
   if(window.matchMedia('(min-width: 768px)').matches){
-    $(".serviceList section").removeClass("col-xs-offset-2")
+    $(".serviceList section").removeClass("col-xs-offset-2");
+
+    $(".serviceList .toggable-content").removeAttr("id");
 
     $(".serviceList .gallery-element").filter(function( index ) {
       return index === 0;
