acb8a14092cf032354e92bd3cc768ad580f46c6e
Mario Andrés Correa <a@macool.me>
2016-06-28 11:36:37 -0500

    revise proyectos view on tablet

--- content/themes/fedes/lib/js/proyectos.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-  if(window.matchMedia('(min-width: 768px)').matches){
-    $(".projectList section").removeClass("col-xs-offset-2")
-
-    $(".projectList .gallery-element").filter(function( index ) {
-      return index === 0 || index === 3 ;
-    }).css("border-left","1px solid #003c71")
-  }
- });
