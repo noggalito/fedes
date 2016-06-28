7f67babb7873c80fc45568cc9a122f52011a40aa
Mario Andrés Correa <a@macool.me>
2016-06-28 00:50:35 -0500

    add numbers to seeds

--- config/seed/generic_seed/yaml_bindings.rb
+++ config/seed/generic_seed/yaml_bindings.rb
@@ -14,11 +14,16 @@ class Seed
       end
 
       def lipsum
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        "#{count} Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       end
 
       def lipsum_small
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        "#{count} Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      end
+
+      def count
+        @count ||= 0
+        @count += 1
       end
     end
   end
